424727ffaeabff3b70f2f2b6517e30e462cbb915
Abdul-Rafay2005 <mrabdulrafay7788@gmail.com>
2025-04-13 15:25:02 +0500

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,43 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStackedWidget>
+#include <QLineEdit>      // Add this for QLineEdit
+#include <QPushButton>    // Add this for QPushButton
+#include <QLabel>         // Add this for QLabel
+#include <QScrollArea>    // Add this for QScrollArea
+#include <QHBoxLayout>    // Add this for QHBoxLayout
+#include <QVBoxLayout>    // Add this for QVBoxLayout
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
+private slots:
+    void showHomePage();
+    void showShippingPage();
+    void showCartPage();
+
+private:
+    // UI elements
+    QLineEdit *searchBar;
+    QPushButton *homeButton;
+    QPushButton *shippingButton;
+    QPushButton *cartButton;
+    QPushButton *viewCartButton;
+    QPushButton *checkoutButton;
+
+    // Page navigation
+    QStackedWidget *stackedWidget;
+    void createHomePage();
+    void createShippingPage();
+    void createCartPage();
+
+    // Window controls
+    void createWindowControls(QWidget *parent);
 };
 
 #endif // MAINWINDOW_H
